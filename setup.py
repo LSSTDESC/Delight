b222613c2a1b4ad256782a9b2f6d34a828d8696e
sschmidt23 <sschmidt@physics.ucdavis.edu>
2023-06-13 13:28:17 -0700

    try making sphinx build optional

--- setup.py
+++ setup.py
@@ -6,11 +6,23 @@ from setuptools import setup, find_packages, find_namespace_packages
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 import numpy
-from sphinx.setup_command import BuildDoc
-
-
+# from sphinx.setup_command import BuildDoc
 
 version = '1.0.1'
+
+cmdclassdict = {"build_ext": build_ext}
+cmdopts = {}
+try:
+    from sphinx.setup_command import BuildDoc
+    cmdclassdict['build_sphinx'] = BuildDoc
+    cmdopts['build_sphinx'] = {
+            'project': (None, "delight"),
+            'version': ('setup.py', version),
+            'build_dir': (None, 'docs/_build'),
+            'config_dir': (None, 'docs'),
+            }
+except ImportError:
+    print('WARNING: sphinx not available, not building docs')
 
 args = {
     "libraries": ["m"],
@@ -32,9 +44,9 @@ ext_modules = [
 setup(
   name="delight",
   version=version,
-  cmdclass={"build_ext": build_ext,
-            'build_sphinx': BuildDoc},
- 
+  # cmdclass={"build_ext": build_ext,
+  #           'build_sphinx': BuildDoc},
+  cmdclass = cmdclassdict,
   #packages=find_packages(exclude=['tests','scripts','data']),  
   #packages=['delight'],
   #packages=['delight','delight.interfaces','delight.interfaces.rail'],
@@ -42,12 +54,13 @@ setup(
   package_dir={'delight': './delight','delight.interfaces':'./delight/interfaces','delight.interfaces.rail':'./delight/interfaces/rail'},
   #package_data={'delightdata': ['data/BROWN_SEDs/*.dat', 'data/CWW_SEDs/*.dat','data/FILTERS/*.res']},
   #package_data={'': extra_files},
-  command_options={
-        'build_sphinx': {
-            'project': (None, "delight"),
-            'version': ('setup.py', version),
-            'build_dir': (None, 'docs/_build'),
-            'config_dir': (None, 'docs'),
-            }},
+  command_options=cmdopts,
+  #command_options={
+        #'build_sphinx': {
+            #'project': (None, "delight"),
+            #'version': ('setup.py', version),
+            #'build_dir': (None, 'docs/_build'),
+            #'config_dir': (None, 'docs'),
+            #}},
   install_requires=["numpy", "scipy", "astropy"],
   ext_modules=ext_modules)
