43dba56d67ed021dc976cfa194247feef85ec283
sylvielsstfr <dagoret@lal.in2p3.fr>
2024-10-23 09:44:24 +0200

    update init files

--- setup.py
+++ setup.py
@@ -2,23 +2,28 @@
 
 import numpy
 from Cython.Build import cythonize
-from setuptools import Extension, setup
+from setuptools import Extension, setup,find_packages
 
 ext_modules = [
     Extension(
         "delight.photoz_kernels_cy",
         ["src/delight/photoz_kernels_cy.pyx"],
         include_dirs=[numpy.get_include()],
-        define_macros=[("CYTHON_LIMITED_API", "1")],
-        py_limited_api=True,
+        #define_macros=[("CYTHON_LIMITED_API", "1")],
+        #py_limited_api=True,
     ),
     Extension(
         "delight.utils_cy",
         ["src/delight/utils_cy.pyx"],
         include_dirs=[numpy.get_include()],
-        define_macros=[("CYTHON_LIMITED_API", "1")],
-        py_limited_api=True,
+        #define_macros=[("CYTHON_LIMITED_API", "1")],
+        #py_limited_api=True,
     ),
 ]
 
-setup(ext_modules=cythonize(ext_modules), include_dirs=[numpy.get_include()])
+setup(ext_modules=cythonize(ext_modules), 
+      include_dirs=[numpy.get_include()],
+      packages=find_packages(where="src"),
+      package_dir={"": "src"},
+      )
+
--- src/delight/__init__.py
+++ src/delight/__init__.py
@@ -1,12 +1,14 @@
-from . import io
 from . import hmc
+from . import io
 from . import photoz_gp
 from . import photoz_kernels
 from . import posteriors
 from . import priors
 from . import sedmixture
 from . import utils
+from . import photoz_kernels_cy
+from . import utils_cy
 
-__all__ = ["io","hmc","photoz_gp","photoz_kernels","posteriors","priors","sedmixture","utils"] 
-
-
+__all__ = ["io", "hmc", "photoz_gp", "photoz_kernels", "posteriors", 
+           "priors", "sedmixture", "utils",
+           "photoz_kernels_cy","utils_cy"]
