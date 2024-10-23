6a52047249a57936c2a97fa840ae67a1b1ca6d89
Dagoret Campagne Sylvie <sylvie.dagoret-campagne@ijclab.in2p3.fr>
2024-10-23 12:32:31 +0200

    remove the delight dir in parallel to src

--- delight/__init__.py
+++
@@ -1,9 +0,0 @@
-__all__ = ["io","hmc","photoz_gp","photoz_kernels","posteriors","priors","sedmixture","utils"] 
-from . import io
-from . import hmc
-from . import photoz_gp
-from . import photoz_kernels
-from . import posteriors
-from . import priors
-from . import sedmixture
-from . import utils
