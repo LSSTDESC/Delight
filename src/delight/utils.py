replace scipy derivative function, fix tests, skip test

src/delight/utils.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-from scipy.misc import derivative
-
+# derivative removed from scipy, replace with numdifftools
+# from scipy.misc import derivative
+import numdifftools as nd
 
 class approx_DL():
     """
@@ -254,7 +255,9 @@ def derivative_test(x0, fun, fun_grad, relative_accuracy,
             x = 1*x0
             x[i] = v
             return fun(x)
-        grads2 = derivative(f, x0[i], dx=dxfac*x0[i], order=order, n=n)
+        # grads2 = derivative(f, x0[i], dx=dxfac*x0[i], order=order, n=n)
+        deriv = nd.Derivative(f, step=dxfac*x0[i], order=order, n=n)
+        grads2 = deriv(x0[i])
         if superverbose:
             print(i, 'analytical:', grads[i], 'numerical:', grads2)
         if np.abs(grads2) >= lim:

tests/test_priors.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 from delight.priors import *
-from scipy.misc import derivative
+# from scipy.misc import derivative
 from delight.utils import derivative_test
 import pytest
 
@@ -114,7 +114,7 @@ def test_MultiTypePopulationPrior():
 
             relative_accuracy = 0.01
             derivative_test(theta, prob, prob_grad, relative_accuracy,
-                            dxfac=1e-2, order=15, lim=1e-4, superverbose=True)
+                            dxfac=1e-2, order=14, lim=1e-4, superverbose=True)
 
     def prob(x):
         mod2 = deepcopy(mod)
@@ -129,5 +129,5 @@ def test_MultiTypePopulationPrior():
     relative_accuracy = 0.01
     print(prob_grad(theta))
     derivative_test(theta, prob, prob_grad, relative_accuracy,
-                    dxfac=1e-1, order=15, lim=1e6, superverbose=True)
+                    dxfac=1e-1, order=14, lim=1e6, superverbose=True)
     # assert 0